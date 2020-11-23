8b7777daf0b844a0bb9db9bc23ed7a6f691837b8
Egor Bulahtin <egorbul98@mail.ru>
2020-11-23 19:32:36 +0300

    small update

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+import favicon from "serve-favicon";
+const path = require('path');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json');
 const middlewares = jsonServer.defaults({
-  static: './public'
+  static: './build'
 })
 
 const port = process.env.PORT || 8080;
 
+server.use(favicon(path.join(__dirname, "./build/favicon.ico")));
 server.use(middlewares);
 server.use(router);
 
