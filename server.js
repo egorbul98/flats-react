da0b83526555703aea80d8979694bff86801c017
Egor Bulahtin <egorbul98@mail.ru>
2020-11-23 19:22:36 +0300

    поменял на static:public

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json');
 const middlewares = jsonServer.defaults({
-  static: './build'
+  static: './public'
 })
 
 const port = process.env.PORT || 8080;
