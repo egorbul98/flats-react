46934d96973573fe8da0164cc6530d0559c9a612
Egor Bulahtin <egorbul98@mail.ru>
2020-11-23 18:51:35 +0300

    fix

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json');
-const middlewares = jsonServer.defaults({ static: './build' })
+const middlewares = jsonServer.defaults({
+  static: './build'
+})
 
 const port = process.env.PORT || 8080;
 
