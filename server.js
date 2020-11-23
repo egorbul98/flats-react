c085dc436831a80157a5a8a319a001da53a02179
Egor Bulahtin <egorbul98@mail.ru>
2020-11-23 16:36:22 +0300

    serv chg

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json');
-const middlewares = jsonServer.defaults({
-  static: './build'
-})
+const middlewares = jsonServer.defaults({ static: './build' })
 
 const port = process.env.PORT || 3004;
 
