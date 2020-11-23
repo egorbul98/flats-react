a50ececa2233eb9f8221943ef169dfeacf0fffc8
Egor Bulahtin <egorbul98@mail.ru>
2020-11-23 15:28:32 +0300

    port change

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const middlewares = jsonServer.defaults({
   static: './build'
 })
 
-const port = proccess.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 server.use(middlewares);
 server.use(router);
